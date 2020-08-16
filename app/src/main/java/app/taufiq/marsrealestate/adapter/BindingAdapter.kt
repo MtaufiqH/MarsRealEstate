b9576dbe2bf820093653a5afe0182bff704d5694
M. Taufiq Hidayat <mtaufiqh62@gmail.com>
2020-08-16 23:23:11 +0800

    add error handling and status of the connection

--- app/src/main/java/app/taufiq/marsrealestate/adapter/BindingAdapter.kt
+++ app/src/main/java/app/taufiq/marsrealestate/adapter/BindingAdapter.kt
@@ -1,11 +1,13 @@
 package app.taufiq.marsrealestate.adapter
 
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import app.taufiq.marsrealestate.R
 import app.taufiq.marsrealestate.remote.MarsProperty
+import app.taufiq.marsrealestate.viewmodel.MarsApiStatus
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 
@@ -31,4 +33,23 @@ fun bindImage(imgView: ImageView, imgUrl: String?){
 fun bindRecyclerView(recyclerView: RecyclerView, data: List<MarsProperty>?){
     val adapter = recyclerView.adapter as PhotoGridAdapter
     adapter.submitList(data)
+}
+
+@BindingAdapter("marsApiStatus")
+fun bindStatus(statusImageView: ImageView, status: MarsApiStatus){
+    when(status){
+        MarsApiStatus.LOADING -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.loading_animation)
+        }
+
+        MarsApiStatus.ERROR -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.ic_connection_error )
+        }
+
+        MarsApiStatus.DONE -> {
+            statusImageView.visibility = View.GONE
+        }
+    }
 }
